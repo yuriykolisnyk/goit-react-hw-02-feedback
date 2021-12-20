5bec8420fda571aaedefced8203f333f11449178
yuriykolisnyk <ykolisnyk@gmail.com>
2021-12-20 15:27:29 +0200

    add 3 buttons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Feedback from './components/feedback';
 const App = () => {
   return (
     <>
-      <h1>Feedback</h1>
+      <h1>Please leave feedback</h1>
       <Feedback />
     </>
   );
--- src/components/feedback.js
+++ src/components/feedback.js
@@ -1,27 +1,47 @@
 import React from 'react';
 
 class Feedback extends React.Component {
-  handleIncrement() {
-    console.log('click add');
-  }
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  handleNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  handleBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
 
   render() {
     return (
       <div>
-        <span>0</span>
         <div>
-          <button type="button" onClick={this.handleIncrement}>
-            Add 1
+          <button type="button" onClick={this.handleGood}>
+            Good
           </button>
-          <button
-            type="button"
-            onClick={() => {
-              console.log('click delete');
-            }}
-          >
-            Delete 1
+          <button type="button" onClick={this.handleNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.handleBad}>
+            Bad
           </button>
         </div>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
       </div>
     );
   }
